1727e4c5185f148caf5a74c8e1022f073b6e1183
rojdasarikamis <rojdasarikamis@gmail.com>
2016-11-10 15:02:19 +0200

    Add files via upload

--- src/main/java/edu/bim207/App.java
+++ src/main/java/edu/bim207/App.java
@@ -1,14 +1,12 @@
 package edu.bim207;
-import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 
-/**
- * @author ROJDA SARİKAMIŞ
- */
 public class App {
 
     public static void main(String[] args) throws IOException {
@@ -17,16 +15,39 @@ public class App {
 
         System.out.println("Fetching: " + url);
 
-        Document document = Jsoup.connect(url).get();
-        Elements elements = document.select("a[href]");
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("a[href]");
 
 
-        for (Element element : elements)
-        {
-            String s = element.attr("abs:href").trim();
+        for (Element link : links) {
+            String s = link.attr("abs:href").trim();
 
             if (s.startsWith("mailto:"))
-                System.out.println(s.substring(7));
+
+                if (s.contains("cc") || s.contains("bcc")) {
+
+                    String[] parts = s.split("\\?|&");
+                    String part1 = parts[0];
+                    String part2 = parts[1];
+                    String part3 = parts[2];
+
+                    System.out.println(part1.substring(7));
+                    System.out.println(part2.substring(3));
+                    System.out.println(part3.substring(4));
+
+
+                } else if (s.contains("?")) {
+                    String[] parts = s.split("\\?");
+                    String part1 = parts[0];
+
+                    System.out.println(part1.substring(7));
+
+
+                } else {
+
+                    System.out.println(s.substring(7));
+
+                }
         }
     }
 }
